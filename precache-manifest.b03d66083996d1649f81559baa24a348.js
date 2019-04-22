d4749c5e2c75ba59e98274d0fda1f3412cf3a44b
Patrick Weygand <patrick.weygand@gmail.com>
2019-04-21 21:51:17 -0700

    Updates

---
+++ precache-manifest.b03d66083996d1649f81559baa24a348.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6219dcedcd28facccbd2",
+    "url": "/ft/static/js/runtime~main.5cf92461.js"
+  },
+  {
+    "revision": "e218158198dd3e24539d",
+    "url": "/ft/static/js/main.36209d0f.chunk.js"
+  },
+  {
+    "revision": "5a516702cc8682739ed2",
+    "url": "/ft/static/js/2.7a165e78.chunk.js"
+  },
+  {
+    "revision": "e218158198dd3e24539d",
+    "url": "/ft/static/css/main.0f73192a.chunk.css"
+  },
+  {
+    "revision": "c9095859072e7b3308bb2f68f3679167",
+    "url": "/ft/index.html"
+  }
+];
