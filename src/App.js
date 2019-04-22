7437bc52eaeffa42e6fadc01007cdc8e7abc2f35
Patrick Weygand <patrick.weygand@gmail.com>
2019-04-21 17:09:20 -0700

    basic filtering and display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import products from './products.json'
+const seriesById = {}
+const productById = {}
+const seriesTypes = [{seriesId: ''}]
+const productTypes = [{prodTypeId: ''}]
+products.documents.forEach(({series, seriesId, productType, prodTypeId}) => {
+  if (!seriesById[seriesId]) {
+    seriesById[seriesId] = true
+    seriesTypes.push({seriesId, series})
+  }
+  if (!productById[prodTypeId]) {
+    productById[prodTypeId] = true
+    productTypes.push({prodTypeId, productType})
+  }
+})
 
 class App extends Component {
+  state = {
+    series: '',
+    productType: ''
+  }
+  onSeriesFilterChange = e => this.setState({series: e.target.value})
+  onProductFilterChange = e => this.setState({productType: e.target.value})
+  activeProducts () {
+    return products.documents.filter(doc => 
+      (!this.state.series || doc.seriesId === this.state.series)
+      && (!this.state.productType || doc.prodTypeId === this.state.productType))
+  }
   render () {
+    const activeProducts = this.activeProducts()
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+        <span>{activeProducts.length}</span>
+        <select onChange={this.onSeriesFilterChange}>
+          {
+            seriesTypes.map(({seriesId, series}) => <option key={seriesId} value={seriesId}>{series}</option>)
+          }
+        </select>
+        <select onChange={this.onProductFilterChange}>
+          {
+            productTypes.map(({prodTypeId, productType}) => <option key={prodTypeId} value={prodTypeId}>{productType}</option>)
+          }
+        </select>
+        <ul className='products'>
+          {
+            activeProducts.map(doc => {
+              const uniq = Array.from(new Set(doc.images))
+              return <li className='product' key={doc.productNo}>{
+                uniq.map(i => <img key={doc.productNo + i} src={i} />)
+              }<span className='product__name'>{ doc.productDisplayName_en }</span>
+              </li>
+            })
+          }
+        </ul>
       </div>
     )
   }
